b29812780dd3e39b83bc12c1800ac22853470967
kevin 11 <kelvinken5282@gmail.com>
2025-08-19 22:49:23 +0300

    Added Footer component with social media icons and imported in App.jsx

--- Frontend/src/component/Footer.jsx
+++ Frontend/src/component/Footer.jsx
@@ -1,20 +1,47 @@
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import {
+  FaInstagram,
+  FaTiktok,
+  FaWhatsapp,
+  FaYoutube,
+} from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className="bg-gray-900 text-white py-4 mt-8">
       <div className="container mx-auto flex justify-center gap-6">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebook size={28} className="hover:text-emerald-400 transition" />
+   
+
+        <a
+          href="https://www.instagram.com/a.connectcollections?igsh=eWRsdTcxNzc1c3J2"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram
+            size={28}
+            className="hover:text-emerald-400 transition"
+          />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-          <FaTwitter size={28} className="hover:text-emerald-400 transition" />
+
+        <a
+          href="https://www.tiktok.com/@a.connectcollections?_t=8lbIVfaVXu9&_r=1 "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaTiktok size={28} className="hover:text-emerald-400 transition" />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagram size={28} className="hover:text-emerald-400 transition" />
+        <a
+          href="https://wa.me/c/2347071111056"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaWhatsapp size={28} className="hover:text-emerald-400 transition" />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-          <FaLinkedin size={28} className="hover:text-emerald-400 transition" />
+        <a
+          href="https://www.youtube.com/@a.connectcollections "
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaYoutube size={28} className="hover:text-emerald-400 transition" />
         </a>
       </div>
       <div className="text-center mt-2 text-sm text-gray-400">
